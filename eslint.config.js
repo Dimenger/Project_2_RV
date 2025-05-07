7041e0d72ec07a71f1b608899a396badcc7c6393
Dmitry Seregin <dimenerDS@gmail.com>
2025-05-07 15:29:41 +0400

    Добавили настройки для prittier, editorconfig

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,38 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-config-prettier'; // Импортируем prettier config
+import prettierPlugin from 'eslint-plugin-prettier'; // Импортируем plugin
 
 export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-]
+	{ ignores: ['dist'] },
+	{
+		files: ['**/*.{js,jsx}'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				ecmaFeatures: { jsx: true },
+				sourceType: 'module',
+			},
+		},
+		plugins: {
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+			prettier: prettierPlugin, // Подключаем плагин prettier
+		},
+		rules: {
+			...js.configs.recommended.rules,
+			...reactHooks.configs.recommended.rules,
+			...prettier.configs['prettier'].rules, // Используем правила Prettier
+			'prettier/prettier': 'error', // Ошибки при нарушении форматов Prettier
+			'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+			'react-refresh/only-export-components': [
+				'warn',
+				{ allowConstantExport: true },
+			],
+		},
+	},
+];
