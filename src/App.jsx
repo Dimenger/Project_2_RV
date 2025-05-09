516bcd16a9ed69429961d3d339609e42e0647499
Dmitry Seregin <dimenerDS@gmail.com>
2025-05-09 20:19:37 +0400

    Добавлен элемент с текущем значением года

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
+function currentYear() {
+	return new Date().getFullYear();
+}
+
 export function App() {
 	const [count, setCount] = useState(0); // Счётчик кликов
 	const [currentTime, setCurrentTime] = useState(new Date().toLocaleString()); // Текущее время
+	const [currentYearValue, setCurrentYearValue] = useState(currentYear()); // State для текущего года
 
 	// Хук для обновления текущего времени каждую секунду
 	useEffect(() => {
@@ -37,6 +42,9 @@ export function App() {
 			<p className="read-the-docs">
 				Click on the Vite and React logos to learn more
 			</p>
+			<div className="cardYear">
+				<p>Год: {currentYearValue}</p> {/* Добавляем отображение текущего года */}
+			</div>
 		</>
 	);
 }
